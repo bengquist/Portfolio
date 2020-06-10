fba1ea0b2dfdeeaa16af3d77d2a8ede011cde8db
bengquist <engquist_blake@yahoo.com>
2020-06-10 15:29:19 -0500

    added necessary pages

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,12 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SkillsPage: React.FC = () => (
-  <Layout headerTitle="skills">
-    <SEO title="skills" />
-    <p>Welcome to skills page</p>
+const ContactPage: React.FC = () => (
+  <Layout headerTitle="contact">
+    <SEO title="contact" />
+    <p>Welcome to contact page</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SkillsPage
+export default ContactPage
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -3,12 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SkillsPage: React.FC = () => (
-  <Layout headerTitle="skills">
-    <SEO title="skills" />
-    <p>Welcome to skills page</p>
+const ProjectsPage: React.FC = () => (
+  <Layout headerTitle="projects">
+    <SEO title="projects" />
+    <p>Welcome to projects page</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SkillsPage
+export default ProjectsPage
