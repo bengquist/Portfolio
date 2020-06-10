c9af75ba3dda2a4f54825c38c8d03dc0d97adf55
bengquist <engquist_blake@yahoo.com>
2020-06-10 16:18:06 -0500

    messing around with gatsby image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,34 @@
-import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 import Layout from "../components/layout"
+import ProfileImage from "../components/ProfileImage"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout headerTitle="about">
     <SEO title="about" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/skills/">Go to page 2</Link>
+
+    <Container>
+      <ProfileImage />
+      <Info>
+        <h1>Full Stack Developer</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad tempore
+          voluptates recusandae veritatis earum cum error nisi laudantium ipsa
+          unde.
+        </p>
+      </Info>
+    </Container>
   </Layout>
 )
 
 export default IndexPage
 
 const Logo = styled.div``
+
+const Container = styled.div`
+  max-width: 300px;
+  display: flex;
+`
+
+const Info = styled.section``
